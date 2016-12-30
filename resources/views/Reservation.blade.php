c1d936e60ddab88ea592e30aecf1fb88c53210b9
smakrariya <smakrariya@gmail.com>
2016-12-30 18:59:11 +0530

    back

--- resources/views/Reservation.blade.php
+++ resources/views/Reservation.blade.php
@@ -69,9 +69,6 @@
 
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('/home') }}">Home</a></li>
-            </ul>
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <meta http-equiv="refresh" content="1;url=/reservation">
+    <script type="text/javascript">
+        window.location.href="/reservation";
+    </script>
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
